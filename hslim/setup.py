74d53bd2c36c4803899bea97c06d892c0651a1d6
Raynes, Jared (A&F, Werribee SnydRd) <Jared.Raynes@csiro.au>
2019-02-08 12:33:54 +1100

    setup of hslim package

---
+++ hslim/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='hslim',
+      version='0.1',
+      description='Program for the analysis of Order-promoting, hydrophobic short linear motifs (H-SLiMs)',
+      url='https://github.com/jaredraynes/HSLIM',
+      author='Jared Raynes',
+      author_email='jared.raynes@csiro.au',
+      license='BSD 3-Clause "New" or "Revised" License',
+      packages=['hslim'],
+      zip_safe=False)
